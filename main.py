cd41e4784a40aef8a2ca286aa1afd94ee8713c09
Michał Kozłowski <kozolex@gmail.com>
2018-10-27 10:53:25 +0200

    Add preproces file

--- main.py
+++ main.py
@@ -7,34 +7,20 @@ import cv2
 import os
 import sys
 
-
 def listDirectory(directory, fileExtList):
     u"zwraca listę obiektów zawierających metadane dla plików o podanych rozszerzeniach"
-    fileList = [os.path.normcase(f) for f in os.listdir(directory)]
-    if len(fileList)==0:
-        fileList = [os.path.join(directory, f) for f in fileList \
-                    if os.path.splitext(f)[1] in fileExtList]
+    fileList = os.listdir(directory)
+    fileList = [os.path.join(directory, f) for f in fileList \
+                if os.path.splitext(f)[1] in fileExtList]
     return fileList
 
-#listFolders = listDirectory("D:\\ziarno2\\TIFF", [".tif"])
-#listFiles = listDirectory("D:/ziarno2/TIFF/Wybity", [".tif"])
-sourceDir = "D:/Doktorat/noweFotyZiaren/uszkodzenia"
-extentionFile = ".tif"
-listFiles = listDirectory(sourceDir, [extentionFile])
-
-if len(listFiles) == 0:
-    print("No files inside")
-for listFolders in listFiles:
-    listFoldersActual = sourceDir + "/" + str(listFolders)
-    listFiles2 = listDirectory(listFoldersActual, [extentionFile])
-    """for imagesss in listFiles2:
-        print(imagesss)
-    cv2.waitKey(10000)"""
-findText = "log1.tif"
+#listaKat = listDirectory("D:\\ziarno2\\TIFF", [".tif"])
+listaKatLog1 = listDirectory("G:/ZIARNA_SKAN/NoweStanowisko/180708/Zdrowe", [".tif"])
+findText = "log2.tif"
 imgToBigCounter = 0 
 mylist = []
 #Create new list with only tif RGB images
-for i in listFiles2:
+for i in listaKatLog1:
     if findText in i:
         #print(i)
         mylist.append(i)
@@ -44,15 +30,15 @@ for idList in range(0,len(mylist)):
 #04336_t.png_log1.tif ID[-20:-15] TYPE[-14:-13] 
     imgID = imgStr[-20:-15]                 #Grain ID 
     imgCamSide = imgStr[-14:-13]            #What side of camera b or t (back / top)
-    image1= cv2.imread(listFoldersActual + mylist[idList])      #read first image to matrix
+    image1= cv2.imread(mylist[idList])      #read first image to matrix
     if idList<=len(mylist):
         idList = idList+1                       #incremet idList to find second image
                         #Counter how many image is too big to target resolution
     if imgID in mylist[idList]:
-        image2= cv2.imread(listFoldersActual + mylist[idList])
-        print(mylist[idList-1]+ "\n" + mylist[idList])
+        image2= cv2.imread(mylist[idList])
+        #print(mylist[idList-1]+ "\n" + mylist[idList])
 
-        print(imgStr[-5:-4])
+        #print(imgStr[-5:-4])
         imgStrCheck = imgStr[:-5] + "2" + imgStr[-4:]
         image1Check = cv2.imread(imgStrCheck) 
         #print(imgStrCheck)
@@ -74,16 +60,18 @@ for idList in range(0,len(mylist)):
             merageImg = np.zeros((hMax, wMax*2, 3), np.uint8)
 
             if image1Check[h1//2,w1//2][2]==255:
-                print("switch")
+                #print("switch")
                 merageImg [hPadding2: h2 + hPadding2, wPadding2+wPadding1:w2 + wPadding2+wPadding1] = image2
                 merageImg [hPadding1: h1 + hPadding1, w2+wPadding2+wPadding1:w2+w1+wPadding2+wPadding1] = image1
             else:
                 merageImg [hPadding1: h1 + hPadding1, wPadding2+wPadding1:w1 + wPadding2+wPadding1] = image1
                 merageImg [hPadding2: h2 + hPadding2, w1+wPadding2+wPadding1:w1+w2+wPadding2+wPadding1] = image2
 
-            cv2.imshow("Merage Image", merageImg)
-            cv2.imwrite("d:/test/a/test.png",merageImg)
-            cv2.waitKey(500)
+            #cv2.imshow("Merage Image", merageImg)
+            cv2.imwrite("D:/ziarno2/PNG/ZielZadesz/"+imgID+".png",merageImg)
+            #cv2.waitKey(500)
+
+
 
 
 
---
+++ test.py
@@ -0,0 +1,2 @@
+imie = raw_input("Podaj swoje imie: ")
+print (imie)
